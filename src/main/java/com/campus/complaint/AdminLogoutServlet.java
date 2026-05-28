44514c8302fa03a23b01dfc974945f6b69920e1d
Suhas Gowda <suhasgowda636227@gmail.com>
2026-05-28 12:18:50 +0530

    loin page changes

---
+++ src/main/java/com/campus/complaint/AdminLogoutServlet.java
@@ -0,0 +1,25 @@
+package com.campus.complaint;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/AdminLogoutServlet")
+public class AdminLogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // destroys admin session
+        }
+
+        response.sendRedirect("admin_login.html?msg=Logged out successfully.");
+    }
+}
