b09aa761fa4a7113024446b4a4bdc0ca71ddcbc5
Suhas Gowda <suhasgowda636227@gmail.com>
2026-05-28 19:00:06 +0530

    Fix Aiven SSL connection - use Properties for SSL config, add serverTimezone

--- src/main/java/com/campus/complaint/DBConnection.java
+++ src/main/java/com/campus/complaint/DBConnection.java
@@ -2,13 +2,26 @@ package com.campus.complaint;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection {
     public static Connection getConnection() throws Exception {
         String url  = System.getenv("DB_URL");
         String user = System.getenv("DB_USER");
         String pass = System.getenv("DB_PASS");
+
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(url, user, pass);
+
+        // SSL properties required for Aiven MySQL
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", pass);
+        props.setProperty("useSSL", "true");
+        props.setProperty("requireSSL", "true");
+        props.setProperty("verifyServerCertificate", "false");
+        props.setProperty("allowPublicKeyRetrieval", "true");
+        props.setProperty("serverTimezone", "Asia/Kolkata");
+
+        return DriverManager.getConnection(url, props);
     }
 }
